432b439a19d98c99cd97b0d7fe13d768682e4de9
Vladimir <Vladimir@Voldemar>
2016-06-07 13:56:41 +0300

    first

---
+++ Lab/src/dd.java
@@ -0,0 +1,20 @@
+
+public class dd {
+	public static void main(String[] args) {	
+		int i = 5, j = 10;
+		String str = "My first string";
+		System.out.println(str);
+		System.out.println("Some text:"+str);
+		int k = i+j;
+		System.out.println("Sum:"+k);
+		while(i<j){ 
+			System.out.println(str);    
+			i++;           
+			}
+		if(i==j){ System.out.println("i="+i+", j="+j+" и они равны"); }
+		else {             
+			System.out.println("i="+i+", j="+j+" и они не равны"); }
+		System.out.println(args[0]);
+	}
+
+}
